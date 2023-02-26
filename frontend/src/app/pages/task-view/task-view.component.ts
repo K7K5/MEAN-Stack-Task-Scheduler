c02989bcb4b56444de5d58c8ad5c83c2b64a7746
Keerthi <keerthikumar8818@gmail.com>
2023-02-26 10:20:24 +0530

    Third try

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Task } from 'src/app/models/task.model';
+import { List } from 'src/app/models/list.model';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  lists: any;
+  tasks: any;
+
+  selectedListId:any;
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) { 
+    //this.lists = [];
+    //this.tasks = [];
+
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        if (params['listId']) {
+          this.selectedListId = params['listId'];
+          this.taskService.getTasks(params['listId']).subscribe((tasks: any) => {
+            this.tasks = tasks;
+          })
+        } else {
+          this.tasks = undefined;
+        }
+      }
+    )
+
+    this.taskService.getLists().subscribe((lists: any) => {
+      this.lists = lists;
+    })
+    
+  }
+  /*log(){
+    this.router.navigate['login'];
+  }*/
+  onTaskClick(task: Task) {
+    // we want to set the task to completed
+    this.taskService.complete(task).subscribe(() => {
+      // the task has been set to completed successfully
+      console.log("Completed successully!");
+      task.completed = !task.completed;
+    })
+  }
+
+  onDeleteListClick() {
+    this.taskService.deleteList(this.selectedListId).subscribe((res: any) => {
+      this.router.navigate(['/lists']);
+      console.log(res);
+    })
+  }
+
+  onDeleteTaskClick(id: string) {
+    this.taskService.deleteTask(this.selectedListId, id).subscribe((res: any) => {
+      this.tasks = this.tasks.filter((val: { _id: string; }) => val._id !== id);
+      console.log(res);
+    })
+  }
+
+}
